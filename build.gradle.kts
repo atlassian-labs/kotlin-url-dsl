a13e098526c58466de5c7d425cbc1e92b7c9a3e8
Daniil Penkin <dpenkin@atlassian.com>
2022-05-23 17:39:49 +1000

    Fix signing configuration
    Read credentials from environment properties.

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,9 +74,10 @@ tasks {
     }
 
     signing {
-        val signingKey: String? by project
-        val signingPassword: String? by project
-        useInMemoryPgpKeys(signingKey, signingPassword)
+        useInMemoryPgpKeys(
+            System.getenv("SIGNING_KEY"),
+            System.getenv("SIGNING_PASSWORD"),
+        )
         sign(publishing.publications["release"])
     }
 }
